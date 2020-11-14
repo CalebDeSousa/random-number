b1d79ba3a456183d493284e4d669c1631199f985
CalebDeSousa <71708632+CalebDeSousa@users.noreply.github.com>
2020-11-13 23:35:32 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-(rando) = (randint(-20, 20))
-
-if (rando >=0) and (rando < 10) or (rando >=15) and (rando < 18):
-    basic.show_number(rando) 
+rando = randint(-20, 20)
+if rando >= 0 and rando < 10 or rando >= 15 and rando < 18:
+    basic.show_number(rando)
